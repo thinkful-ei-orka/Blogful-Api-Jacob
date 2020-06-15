19a7a25160c0b13b0a0ddc2127bb956f63e74185
JZPoteat <jzpoteat@gmail.com>
2020-06-15 14:27:21 -0400

    added comments and articles folders

---
+++ migrations/002.do.alter_article_style.sql
@@ -0,0 +1,17 @@
+
+
+CREATE TYPE article_category AS ENUM (
+    'Listicle',
+    'How-to',
+    'News',
+    'Interview',
+    'Story'
+);
+
+ALTER TABLE blogful_articles
+  ADD COLUMN
+    style article_category;
+
+
+
+
--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
+const articlesRouter = require('./articles/articles-router');
+const usersRouter = require('./users/users-router');
+const commentsRouter = require('./comments/comments-router');
+
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
@@ -12,10 +16,20 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
+
+app.use('/api/articles', articlesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/comments', commentsRouter);
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!');
 });
 
+app.get('/xss', (req, res) => {
+    res.cookie('secretToken', '1234567890');
+    res.sendFile(__dirname + '/xss-example.html');
+});
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
